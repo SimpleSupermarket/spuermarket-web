e03a2b349625b1b486333c5c59a4eaef1d472354
董文强 <826112946@qq.com>
2018-12-03 16:43:05 +0800

    sql文件

---
+++ smbms.sql
@@ -0,0 +1,198 @@
+/*
+SQLyog Community v13.1.1 (64 bit)
+MySQL - 5.7.17-log : Database - smbms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`smbms` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `smbms`;
+
+/*Table structure for table `smbms_address` */
+
+DROP TABLE IF EXISTS `smbms_address`;
+
+CREATE TABLE `smbms_address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `contact` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '联系人姓名',
+  `addressDesc` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '收货地址明细',
+  `postCode` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '邮编',
+  `tel` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '联系人电话',
+  `createdBy` bigint(20) DEFAULT NULL COMMENT '创建者',
+  `creationDate` datetime DEFAULT NULL COMMENT '创建时间',
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  PRIMARY KEY (`id`),
+  KEY `createdBy` (`createdBy`),
+  KEY `smbms_address_user_id_ibfk` (`user_id`),
+  CONSTRAINT `smbms_address_ibfk_1` FOREIGN KEY (`createdBy`) REFERENCES `smbms_user` (`id`),
+  CONSTRAINT `smbms_address_user_id_ibfk` FOREIGN KEY (`user_id`) REFERENCES `smbms_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `smbms_address` */
+
+insert  into `smbms_address`(`id`,`contact`,`addressDesc`,`postCode`,`tel`,`createdBy`,`creationDate`,`user_id`) values 
+(1,'王丽','北京市东城区东交民巷44号','100010','13678789999',1,'2016-04-13 00:00:00',1),
+(2,'张红丽','北京市海淀区丹棱街3号','100000','18567672312',1,'2016-04-13 00:00:00',1),
+(3,'任志强','北京市东城区美术馆后街23号','100021','13387906742',1,'2016-04-13 00:00:00',1),
+(4,'曹颖','北京市朝阳区朝阳门南大街14号','100053','13568902323',1,'2016-04-13 00:00:00',2),
+(5,'李慧','北京市西城区三里河路南三巷3号','100032','18032356666',1,'2016-04-13 00:00:00',3),
+(6,'王国强','北京市顺义区高丽营镇金马工业区18号','100061','13787882222',1,'2016-04-13 00:00:00',3);
+
+/*Table structure for table `smbms_bill` */
+
+DROP TABLE IF EXISTS `smbms_bill`;
+
+CREATE TABLE `smbms_bill` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `billCode` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '账单编码',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品',
+  `goodsCount` int(11) DEFAULT NULL COMMENT '商品数量',
+  `totalPrice` decimal(20,2) DEFAULT NULL COMMENT '商品总额',
+  `isPayment` int(10) DEFAULT NULL COMMENT '是否支付（1：未支付 2：已支付）',
+  `createdBy` bigint(20) DEFAULT NULL COMMENT '创建者（userId）',
+  `creationDate` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `goods_id` (`goods_id`),
+  KEY `createdBy` (`createdBy`),
+  CONSTRAINT `smbms_bill_ibfk_1` FOREIGN KEY (`goods_id`) REFERENCES `smbms_goods` (`id`),
+  CONSTRAINT `smbms_bill_ibfk_3` FOREIGN KEY (`createdBy`) REFERENCES `smbms_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `smbms_bill` */
+
+/*Table structure for table `smbms_goods` */
+
+DROP TABLE IF EXISTS `smbms_goods`;
+
+CREATE TABLE `smbms_goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `goods_code` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '商品编码',
+  `goods_namne` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '商品名称',
+  `price` decimal(20,2) DEFAULT NULL COMMENT '价格',
+  `unit` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '单位',
+  `provider_id` bigint(20) DEFAULT NULL COMMENT '供应商ID',
+  `gstock` int(11) DEFAULT NULL COMMENT '库存',
+  `createdBy` bigint(20) DEFAULT NULL COMMENT '创建者(userid)',
+  `creationDate` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `provider_id` (`provider_id`),
+  KEY `createdBy` (`createdBy`),
+  CONSTRAINT `smbms_goods_ibfk_1` FOREIGN KEY (`provider_id`) REFERENCES `smbms_provider` (`id`),
+  CONSTRAINT `smbms_goods_ibfk_2` FOREIGN KEY (`createdBy`) REFERENCES `smbms_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `smbms_goods` */
+
+/*Table structure for table `smbms_provider` */
+
+DROP TABLE IF EXISTS `smbms_provider`;
+
+CREATE TABLE `smbms_provider` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `proCode` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '供应商编码',
+  `proName` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '供应商名称',
+  `proDesc` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '供应商详细描述',
+  `proContact` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '供应商联系人',
+  `proPhone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '联系电话',
+  `proAddress` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '地址',
+  `proFax` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '传真',
+  `createdBy` bigint(20) DEFAULT NULL COMMENT '创建者（userId）',
+  `creationDate` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `createdBy` (`createdBy`),
+  CONSTRAINT `smbms_provider_ibfk_1` FOREIGN KEY (`createdBy`) REFERENCES `smbms_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `smbms_provider` */
+
+insert  into `smbms_provider`(`id`,`proCode`,`proName`,`proDesc`,`proContact`,`proPhone`,`proAddress`,`proFax`,`createdBy`,`creationDate`) values 
+(1,'BJ_GYS001','北京三木堂商贸有限公司','长期合作伙伴，主营产品:茅台、五粮液、郎酒、酒鬼酒、泸州老窖、赖茅酒、法国红酒等','张国强','13566667777','北京市丰台区育芳园北路','010-58858787',1,'2013-03-21 16:52:07'),
+(2,'HB_GYS001','石家庄帅益食品贸易有限公司','长期合作伙伴，主营产品:饮料、水饮料、植物蛋白饮料、休闲食品、果汁饮料、功能饮料等','王军','13309094212','河北省石家庄新华区','0311-67738876',1,'2016-04-13 04:20:40'),
+(3,'GZ_GYS001','深圳市泰香米业有限公司','初次合作伙伴，主营产品：良记金轮米,龙轮香米等','郑程瀚','13402013312','广东省深圳市福田区深南大道6006华丰大厦','0755-67776212',1,'2014-03-21 16:56:07'),
+(4,'GZ_GYS002','深圳市喜来客商贸有限公司','长期合作伙伴，主营产品：坚果炒货.果脯蜜饯.天然花茶.营养豆豆.特色美食.进口食品.海味零食.肉脯肉','林妮','18599897645','广东省深圳市福龙工业区B2栋3楼西','0755-67772341',1,'2013-03-22 16:52:07'),
+(5,'JS_GYS001','兴化佳美调味品厂','长期合作伙伴，主营产品：天然香辛料、鸡精、复合调味料','徐国洋','13754444221','江苏省兴化市林湖工业区','0523-21299098',1,'2015-11-22 16:52:07'),
+(6,'BJ_GYS002','北京纳福尔食用油有限公司','长期合作伙伴，主营产品：山茶油、大豆油、花生油、橄榄油等','马莺','13422235678','北京市朝阳区珠江帝景1号楼','010-588634233',1,'2012-03-21 17:52:07'),
+(7,'BJ_GYS003','北京国粮食用油有限公司','初次合作伙伴，主营产品：花生油、大豆油、小磨油等','王驰','13344441135','北京大兴青云店开发区','010-588134111',1,'2016-04-13 00:00:00'),
+(8,'ZJ_GYS001','慈溪市广和绿色食品厂','长期合作伙伴，主营产品：豆瓣酱、黄豆酱、甜面酱，辣椒，大蒜等农产品','薛圣丹','18099953223','浙江省宁波市慈溪周巷小安村','0574-34449090',1,'2013-11-21 06:02:07'),
+(9,'GX_GYS001','优百商贸有限公司','长期合作伙伴，主营产品：日化产品','李立国','13323566543','广西南宁市秀厢大道42-1号','0771-98861134',1,'2013-03-21 19:52:07'),
+(10,'JS_GYS002','南京火头军信息技术有限公司','长期合作伙伴，主营产品：不锈钢厨具等','陈女士','13098992113','江苏省南京市浦口区浦口大道1号新城总部大厦A座903室','025-86223345',1,'2013-03-25 16:52:07'),
+(11,'GZ_GYS003','广州市白云区美星五金制品厂','长期合作伙伴，主营产品：海绵床垫、坐垫、靠垫、海绵枕头、头枕等','梁天','13562276775','广州市白云区钟落潭镇福龙路20号','020-85542231',1,'2016-12-21 06:12:17'),
+(12,'BJ_GYS004','北京隆盛日化科技','长期合作伙伴，主营产品：日化环保清洗剂，家居洗涤专卖、洗涤用品网、墙体除霉剂、墙面霉菌清除剂等','孙欣','13689865678','北京市大兴区旧宫','010-35576786',1,'2014-11-21 12:51:11'),
+(13,'SD_GYS001','山东豪克华光联合发展有限公司','长期合作伙伴，主营产品：洗衣皂、洗衣粉、洗衣液、洗洁精、消杀类、香皂等','吴洪转','13245468787','山东济阳济北工业区仁和街21号','0531-53362445',1,'2015-01-28 10:52:07'),
+(14,'JS_GYS003','无锡喜源坤商行','长期合作伙伴，主营产品：日化品批销','周一清','18567674532','江苏无锡盛岸西路','0510-32274422',1,'2016-04-23 11:11:11'),
+(15,'ZJ_GYS002','乐摆日用品厂','长期合作伙伴，主营产品：各种中、高档塑料杯，塑料乐扣水杯（密封杯）、保鲜杯（保鲜盒）、广告杯、礼品杯','王世杰','13212331567','浙江省金华市义乌市义东路','0579-34452321',1,'2016-08-22 10:01:30');
+
+/*Table structure for table `smbms_role` */
+
+DROP TABLE IF EXISTS `smbms_role`;
+
+CREATE TABLE `smbms_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `roleCode` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '角色编码',
+  `roleName` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '角色名称',
+  `createdBy` bigint(20) DEFAULT NULL COMMENT '创建者',
+  `creationDate` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `createdBy` (`createdBy`),
+  CONSTRAINT `smbms_role_ibfk_1` FOREIGN KEY (`createdBy`) REFERENCES `smbms_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `smbms_role` */
+
+insert  into `smbms_role`(`id`,`roleCode`,`roleName`,`createdBy`,`creationDate`) values 
+(1,'SMBMS_ADMIN','系统管理员',1,'2016-04-13 00:00:00'),
+(2,'SMBMS_MANAGER','经理',1,'2016-04-13 00:00:00'),
+(3,'SMBMS_EMPLOYEE','普通员工',1,'2016-04-13 00:00:00');
+
+/*Table structure for table `smbms_user` */
+
+DROP TABLE IF EXISTS `smbms_user`;
+
+CREATE TABLE `smbms_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `userCode` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '用户编码',
+  `userName` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '用户名称',
+  `userPassword` char(32) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '用户密码',
+  `gender` int(10) DEFAULT NULL COMMENT '性别（1:女、 2:男）',
+  `birthday` date DEFAULT NULL COMMENT '出生日期',
+  `phone` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '手机',
+  `address` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '地址',
+  `role_id` bigint(20) DEFAULT NULL COMMENT '用户角色（取自角色表-角色id）',
+  `createdBy` bigint(20) DEFAULT NULL COMMENT '创建者（userId）',
+  `creationDate` datetime DEFAULT NULL COMMENT '创建时间',
+  `state` int(11) DEFAULT '0' COMMENT '状态',
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `createdBy` (`createdBy`),
+  CONSTRAINT `smbms_user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `smbms_role` (`id`),
+  CONSTRAINT `smbms_user_ibfk_2` FOREIGN KEY (`createdBy`) REFERENCES `smbms_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `smbms_user` */
+
+insert  into `smbms_user`(`id`,`userCode`,`userName`,`userPassword`,`gender`,`birthday`,`phone`,`address`,`role_id`,`createdBy`,`creationDate`,`state`) values 
+(1,'admin','系统管理员','1234567',1,'1983-10-10','13688889999','北京市海淀区成府路207号',1,1,'2013-03-21 16:52:07',0),
+(2,'liming','李明','0000000',2,'1983-12-10','13688884457','北京市东城区前门东大街9号',2,1,'2014-12-31 19:52:09',0),
+(3,'hanlubiao','韩路彪','0000000',2,'1984-06-05','18567542321','北京市朝阳区北辰中心12号',2,1,'2014-12-31 19:52:09',0),
+(6,'zhanghua','张华','0000000',1,'1983-06-15','13544561111','北京市海淀区学院路61号',3,1,'2013-02-11 10:51:17',0),
+(7,'wangyang','王洋','0000000',2,'1982-12-31','13444561124','北京市海淀区西二旗辉煌国际16层',3,1,'2014-06-11 19:09:07',0),
+(8,'zhaoyan','赵燕','0000000',1,'1986-03-07','18098764545','北京市海淀区回龙观小区10号楼',3,1,'2016-04-21 13:54:07',0),
+(10,'sunlei','孙磊','0000000',2,'1981-01-04','13387676765','北京市朝阳区管庄新月小区12楼',3,1,'2015-05-06 10:52:07',0),
+(11,'sunxing','孙兴','0000000',2,'1978-03-12','13367890900','北京市朝阳区建国门南大街10号',3,1,'2016-11-09 16:51:17',0),
+(12,'zhangchen','张晨','0000000',1,'1986-03-28','18098765434','朝阳区管庄路口北柏林爱乐三期13号楼',3,1,'2016-08-09 05:52:37',0),
+(13,'dengchao','邓超','0000000',2,'1981-11-04','13689674534','北京市海淀区北航家属院10号楼',3,1,'2016-07-11 08:02:47',0),
+(14,'yangguo','杨过','0000000',2,'1980-01-01','13388886623','北京市朝阳区北苑家园茉莉园20号楼',3,1,'2015-02-01 03:52:07',0),
+(15,'zhaomin','赵敏','0000000',1,'1987-12-04','18099897657','北京市昌平区天通苑3区12号楼',2,1,'2015-09-12 12:02:12',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
