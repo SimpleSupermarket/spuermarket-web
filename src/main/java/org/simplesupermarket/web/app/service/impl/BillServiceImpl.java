fc6d2de7e93f45a6319464e101fe9f40f67a047d
董文强 <826112946@qq.com>
2018-12-03 14:03:08 +0800

    账单服务的实现类

---
+++ src/main/java/org/simplesupermarket/web/app/service/impl/BillServiceImpl.java
@@ -0,0 +1,15 @@
+package org.simplesupermarket.web.app.service.impl;
+import org.simplesupermarket.web.app.service.AbstractSuperServiceImpl;
+import org.simplesupermarket.web.app.service.BillService;
+import org.simplesupermarket.web.db.model.Bill;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * 
+ * @author 董文强
+ * @date 2018年12月03日
+ * @version 1.0
+ */
+public class BillServiceImpl extends AbstractSuperServiceImpl<Bill> implements BillService {
+ private static final Logger LOGGER = LoggerFactory.getLogger(BillServiceImpl.class);
+}
