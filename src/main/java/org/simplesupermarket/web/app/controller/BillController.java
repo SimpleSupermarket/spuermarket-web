304394b6289129ffe35da56275689ac2bd78010f
董文强 <826112946@qq.com>
2018-12-03 13:44:18 +0800

    创建Controller

---
+++ src/main/java/org/simplesupermarket/web/app/controller/BillController.java
@@ -0,0 +1,12 @@
+package org.simplesupermarket.web.app.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * 
+ * @author 董文强
+ * @date 2018年12月03日
+ * @version 1.0
+ */
+public class BillController {
+ private static final Logger LOGGER = LoggerFactory.getLogger(BillController.class);
+}
