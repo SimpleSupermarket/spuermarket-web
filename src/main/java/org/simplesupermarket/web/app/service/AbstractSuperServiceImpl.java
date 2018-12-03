812f568d50ed2876fb8986bdcb1fb957dc3411e7
董文强 <826112946@qq.com>
2018-12-03 14:02:42 +0800

    添加公共Service公共抽象父类，基本方法封装好

---
+++ src/main/java/org/simplesupermarket/web/app/service/AbstractSuperServiceImpl.java
@@ -0,0 +1,12 @@
+package org.simplesupermarket.web.app.service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * 
+ * @author 董文强
+ * @date 2018年12月03日
+ * @version 1.0
+ */
+public abstract class  AbstractSuperServiceImpl<T> {
+ private static final Logger LOGGER = LoggerFactory.getLogger(AbstractSuperServiceImpl.class);
+}
